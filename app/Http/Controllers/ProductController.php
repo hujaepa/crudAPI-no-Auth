39018800f704e59b468d08be8ff603446b54b7cf
hujaepa <hujaepa@gmail.com>
2022-08-06 14:53:43 +0800

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Product;
+use Illuminate\Http\Request;
+class ProductController extends Controller
+{
+    /*
+     * get all product response
+     */
+
+    public function index()
+    {
+        $products= Product::all();
+        return response()->json($products);
+    }
+
+    /*
+    * get product by id
+    */
+
+    public function show($productId)
+    {
+        $product= Product::find($productId);
+        return response()->json($product);
+    }
+}
---
+++ database/migrations/2022_08_06_055134_create_table_products.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableProducts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_products', function (Blueprint $table) {
+            $table->bigIncrements('product_id');
+            $table->string("product_name");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_products');
+    }
+}
